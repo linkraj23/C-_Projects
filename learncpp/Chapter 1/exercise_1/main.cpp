d8ac019a09c2a2f909f67ab41edf1a9541a3da69
Link_raj23 <raj.kautep71@gmail.com>
2025-09-22 08:58:35 +0530

    day 3

---
+++ learncpp/Chapter 1/exercise_1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter first no: ";
+    cin >> a;
+
+    cout << "Enter second no: ";
+    cin >> b;
+
+    cout << a << " + " << b << " = " << a + b << endl; 
+    cout << a << " - " << b << " = " << a - b << endl;
+    
+    return 0;
+}
